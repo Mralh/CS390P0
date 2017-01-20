52d8b0e2b76e5ab384480c6b48ca9ed12ffaf15c
Mralh <mralhamm@gmail.com>
2017-01-20 01:35:44 -0500

    Rotation and Scale

---
+++ Assets/Resources/Scripts/HWRotPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HWRotPlayer : MonoBehaviour {
+
+    float crosshairAlpha = 0.0f;
+	void FixedUpdate ()
+    {
+        RaycastHit[] ray = Physics.RaycastAll(transform.position, transform.forward, 50, 1 << LayerMask.NameToLayer("Enemy"));
+        foreach (RaycastHit r in ray)
+        {
+            if (r.collider.transform.parent.gameObject.GetComponent<Spookman>() != null)
+            {
+                r.collider.transform.parent.gameObject.GetComponent<Spookman>().gaze();
+                transform.GetChild(0).gameObject.GetComponent<Renderer>().sharedMaterial.color = new Color(1, 1, 1, 0.3f);
+                break;
+            }
+            Debug.Log("Hit");
+        }
+        transform.GetChild(0).gameObject.GetComponent<Renderer>().sharedMaterial.color = new Color(1, 1, 1, 
+            transform.GetChild(0).gameObject.GetComponent<Renderer>().sharedMaterial.color.a - 0.05f);
+    }
+}
---
+++ Assets/Resources/Scripts/Spookman.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spookman : MonoBehaviour
+{
+    public Vector3 playerPos;
+
+    float speed = 4;
+    float maxSpeed = 8;
+
+    int hp = 100;
+
+    List<Vector3> spawnPoints = new List<Vector3>();
+
+    void Start()
+    {
+        Transform points = GameObject.Find("spawnpoints").transform;
+        foreach (Transform t in points)
+        {
+            if (t != points)
+            {
+                spawnPoints.Add(t.position);
+            }
+        }
+    }
+
+	void FixedUpdate ()
+    {
+		if (hp <= 0)
+            respawn();
+
+        if (GetComponent<Rigidbody>().velocity.y < 1 && Mathf.Abs((playerPos - transform.position).magnitude) > 3)
+            GetComponent<Rigidbody>().velocity = (playerPos - transform.position).normalized * speed;
+        transform.LookAt(new Vector3(playerPos.x, transform.position.y, playerPos.z));
+
+	}
+
+    public void gaze()
+    {
+        hp -= 5;
+    }
+
+    void respawn()
+    {
+        speed += 0.5f;
+        if (speed > maxSpeed)
+        {
+            GameObject.Instantiate(Resources.Load<GameObject>("Prefabs/spookyman"),
+                spawnPoints[(int)Random.Range(0, (float)spawnPoints.Count - 0.1f)],
+                Quaternion.identity);
+            speed = 3;
+        }
+        
+        hp = 100;
+        
+        transform.position = spawnPoints[(int)Random.Range(0, (float)spawnPoints.Count - 0.1f)];
+    }
+}
